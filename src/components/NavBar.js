9236a02f33150c21a3abf6068837fde43fe8c9cf
amaar <amaar.ahmednur@student.moringaschool.com>
2023-11-03 12:23:21 +0300

    finished assignment and added spaces between the links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyle = {
+  textDecoration: "none",
+  marginRight: '15px',
+};
+
 function NavBar() {
   return (
     <div className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
+     <NavLink to="/" style={linkStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={linkStyle}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={linkStyle}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={linkStyle}>
+        Actors
+      </NavLink>
     </div>
   );
 }
